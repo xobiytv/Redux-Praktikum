eb9abe4ab841610691a4eb1928e6e6039b95e8d5
unknown <xobiytv.github.io>
2024-01-21 18:09:59 +0500

    Readux parkatikum

---
+++ src/componets/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Main() {
+    return (
+        <div>
+            <h1 className="text-3xl text-center font-bold underline">
+                Hello world!
+            </h1>
+        </div>
+    )
+}
---
+++ src/componets/index.js
@@ -0,0 +1,3 @@
+export {default as Main} from './Main';
+export {default as Register} from './Register';
+export {default as Login} from './Login';
